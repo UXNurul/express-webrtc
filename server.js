74f030989896ef20582551578762a952c757b7d3
Nurul Islam Molla <79696382+UXNurul@users.noreply.github.com>
2024-08-08 23:13:41 +0530

    server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+});
 
 io.on('connection', (socket) => {
     console.log('New client connected');
